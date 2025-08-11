3f55a60fcc4bc4e482c17d0fddd395bc1da5e6c2
Robert P <163090599+RobertParesi@users.noreply.github.com>
2025-08-11 08:06:08 -0700

    Update MonarchMoneyTweaksBeta.user.js
    Signed-off-by: Robert P <163090599+RobertParesi@users.noreply.github.com>

--- beta/MonarchMoneyTweaksBeta.user.js
+++ beta/MonarchMoneyTweaksBeta.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         Monarch Money Tweaks
 // @namespace    http://tampermonkey.net/
-// @version      3.32.09
+// @version      3.32
 // @description  Monarch Tweaks
 // @author       Robert P
 // @match        https://app.monarchmoney.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=monarchmoney.com
 // ==/UserScript==
 
-const version = '3.32.09';
+const version = '3.32';
 const css_currency = 'USD';
 const css_green = 'color: #2a7e3b;',css_red = 'color: #d13415;';
 const graphql = 'https://api.monarchmoney.com/graphql';
@@ -1023,7 +1023,7 @@ async function MenuReportsAccountsGo() {
     MTFlex.TriggerEvents = false;
     MF_SetupDates();
     MF_GridOptions(1,['Hide subtotals','Subtotal on Type','Subtotal on Group']);
-    MF_GridOptions(2,['Standard Report','Personal Statement','Last 6 months with average','Last 12 months with average','This year with average','Last 3 years with average']);
+    MF_GridOptions(2,['Standard Report','Personal Statement','Last 6 months with average','Last 12 months with average','This year with average','Last 3 years by Quarter']);
     MF_GridOptions(4,getAccountGroupInfo());
     MTP = [];
     if(MTFlex.Button1 > 0) MTFlex.Subtotals = true;
