ff028d3fdad5e72f2eeaf5e734c7d587e7eaa76d
Robert P <163090599+RobertParesi@users.noreply.github.com>
2025-09-08 17:27:46 -0700

    Update MonarchMoneyTweaksBeta.user.js

--- beta/MonarchMoneyTweaksBeta.user.js
+++ beta/MonarchMoneyTweaksBeta.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         Monarch Money Tweaks
 // @namespace    http://tampermonkey.net/
-// @version      3.52.14
+// @version      3.52
 // @description  Monarch Tweaks
 // @author       Robert P
 // @match        https://app.monarchmoney.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=monarchmoney.com
 // ==/UserScript==
 
-const version = '3.52.14';
+const version = '3.52';
 const css_currency = 'USD',CRLF = String.fromCharCode(13,10);
 const css_green = 'color: #2a7e3b;',css_red = 'color: #d13415;';
 const graphql = 'https://api.monarchmoney.com/graphql';
